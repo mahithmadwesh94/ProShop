c3550e045c71e690f7918d0b1d728da8e500ad37
Mahith <mahith.madwesh@gmail.com>
2026-02-23 10:33:49 +0530

    Implemented Header,Footer and Homescreen with Product Component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import {Nav, Container, Navbar} from 'react-bootstrap';
+import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import logo from '../assets/logo.png'
+
+const Header = () => {
+  return (
+    <header>
+   <Navbar bg='dark' variant='dark' collapseOnSelect expand="lg">
+    <Container>
+        <Navbar.Brand href='/'>
+        <img src={logo} alt='logo' />
+        ProShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar'/>
+            <Navbar.Collapse id='basic-navbar'>
+                <Nav className='ms-auto'>
+                    <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
+                    <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+       
+    </Container>
+   </Navbar>
+   </header>
+  )
+}
+
+export default Header
