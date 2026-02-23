6b011842308801228e422d6b60e2bcce678f5362
Mahith <mahith.madwesh@gmail.com>
2026-02-23 11:18:54 +0530

    Implemented react-router-dom

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,38 @@
-import {Nav, Container, Navbar} from 'react-bootstrap';
-import {FaShoppingCart, FaUser} from 'react-icons/fa';
-import logo from '../assets/logo.png'
+import { Nav, Container, Navbar } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
-   <Navbar bg='dark' variant='dark' collapseOnSelect expand="lg">
-    <Container>
-        <Navbar.Brand href='/'>
-        <img src={logo} alt='logo' />
-        ProShop</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar'/>
-            <Navbar.Collapse id='basic-navbar'>
-                <Nav className='ms-auto'>
-                    <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-                    <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-       
-    </Container>
-   </Navbar>
-   </header>
-  )
-}
+      <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="logo" />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar" />
+          <Navbar.Collapse id="basic-navbar">
+            <Nav className="ms-auto">
+                <LinkContainer to='/cart'>
+              <Nav.Link>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+              <Nav.Link>
+                <FaUser /> Sign In
+              </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
